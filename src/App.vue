acf030e6e8d5332bbf1f177a537521c19cb1f306
njengagichuhi <njengagichuhi211@gmail.com>
2024-09-30 12:24:03 +0300

    vue_quote

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import randomQuotegen from './components/randomQuotegen.vue';
+</script>
+
+<template>
+  <randomQuotegen />
+
+  <h1></h1>
+</template>
+
+<style scoped>
+
+</style>
